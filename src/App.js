fda6f7bac49b50a20f816ccd7a19f7247cb85850
Paulo Menezes <phgm@cesar.org.br>
2020-03-02 20:29:57 -0300

    feat: select sprites

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import sprite from './sprite.png';
+import sprite from "./sprite.png";
 
 export default class App extends Component {
   state = {
     width: 200,
     height: 189,
+    moves: [],
+    newMove: 5,
     rectangles: [],
+    hoverRectangle: -1,
+    selectedRectangle: -1,
+    hoverMoveRectangleI: -1,
+    hoverMoveRectangleJ: -1,
+    selectMoveRectangleI: -1,
+    selectMoveRectangleJ: -1,
+    canvas: null,
+    context: null
   };
 
   componentDidMount() {
@@ -14,29 +24,39 @@ export default class App extends Component {
 
     image.onload = () => {
       const canvas = this.refs.canvas;
+      const context = canvas.getContext("2d");
+
+      this.setState({
+        canvas,
+        context
+      });
+
       this.rect = canvas.getBoundingClientRect(); // abs. size of element
       this.scaleX = canvas.width / this.rect.width; // relationship bitmap vs. element for X
       this.scaleY = canvas.height / this.rect.height; // relationship bitmap vs. element for Y
 
-      this.reload();
+      this.updateRectangles();
     };
   }
 
-  reload() {
-    const canvas = this.refs.canvas;
-    const context = canvas.getContext('2d');
+  componentDidUpdate() {
+    this.reload();
+  }
+
+  updateRectangles() {
+    const context = this.state.context;
     const image = this.refs.image;
 
     context.clearRect(0, 0, image.clientWidth, image.clientHeight);
     context.drawImage(image, 0, 0);
 
-    context.strokeStyle = 'red';
-    context.beginPath();
-    context.rect(0, 0, +this.state.width, +this.state.height);
-    context.stroke();
-
     const width = +this.state.width;
     const height = +this.state.height;
+
+    context.strokeStyle = "red";
+    context.beginPath();
+    context.rect(0, 0, width, height);
+    context.stroke();
 
     console.log(width, height, this.state);
     let startLine = 0;
@@ -47,14 +67,19 @@ export default class App extends Component {
     const columns = [];
 
     this.setState({
-      rectangles: [],
+      rectangles: []
     });
 
     for (let y = 0; y < 200; y++) {
       let line = true;
       for (let x = 0; x < width; x++) {
         const pixelData = context.getImageData(x, y, 1, 1).data;
-        if (pixelData[0] !== 0 && pixelData[1] !== 0 && pixelData[2] !== 0 && pixelData[3] !== 0) {
+        if (
+          pixelData[0] !== 0 &&
+          pixelData[1] !== 0 &&
+          pixelData[2] !== 0 &&
+          pixelData[3] !== 0
+        ) {
           line = false;
           break;
         }
@@ -93,7 +118,12 @@ export default class App extends Component {
       let line = true;
       for (let y = 0; y < 200; y++) {
         const pixelData = context.getImageData(x, y, 1, 1).data;
-        if (pixelData[0] !== 0 && pixelData[1] !== 0 && pixelData[2] !== 0 && pixelData[3] !== 0) {
+        if (
+          pixelData[0] !== 0 &&
+          pixelData[1] !== 0 &&
+          pixelData[2] !== 0 &&
+          pixelData[3] !== 0
+        ) {
           line = false;
           break;
         }
@@ -169,7 +199,11 @@ export default class App extends Component {
         const dx = p[0] - point[0];
         const dy = p[1] - point[1];
 
-        if ((!found[0] && dx > 0 && dy > 0) || (!found[1] && dx === 0 && dy > 0) || (!found[2] && dx > 0 && dy === 0)) {
+        if (
+          (!found[0] && dx > 0 && dy > 0) ||
+          (!found[1] && dx === 0 && dy > 0) ||
+          (!found[2] && dx > 0 && dy === 0)
+        ) {
           if (dx > 0 && dy > 0) {
             found[0] = true;
           }
@@ -182,7 +216,7 @@ export default class App extends Component {
 
           distances.push({
             d: this.distance(point[0], point[1], p[0], p[1]),
-            p,
+            p
           });
         }
 
@@ -196,12 +230,17 @@ export default class App extends Component {
 
       distances.sort((a, b) => a.d - b.d);
 
-      rectangles.push([point[0], point[1], distances[2].p[0] - point[0], distances[2].p[1] - point[1]]);
+      rectangles.push([
+        point[0],
+        point[1],
+        distances[2].p[0] - point[0],
+        distances[2].p[1] - point[1]
+      ]);
     }
 
     console.log(rectangles);
     this.setState({
-      rectangles,
+      rectangles
     });
 
     // for (const rectangle of rectangles) {
@@ -222,6 +261,87 @@ export default class App extends Component {
     // }
   }
 
+  reload() {
+    const context = this.state.context;
+    const image = this.refs.image;
+
+    context.clearRect(0, 0, this.state.canvas.width, this.state.canvas.height);
+    context.drawImage(image, 0, 0);
+
+    const width = +this.state.width;
+    const height = +this.state.height;
+
+    context.strokeStyle = "red";
+    context.beginPath();
+    context.rect(0, 0, width, height);
+    context.stroke();
+
+    if (this.state.hoverRectangle > -1) {
+      context.strokeStyle = "blue";
+      context.beginPath();
+      context.rect(
+        this.state.rectangles[this.state.hoverRectangle][0],
+        this.state.rectangles[this.state.hoverRectangle][1],
+        this.state.rectangles[this.state.hoverRectangle][2],
+        this.state.rectangles[this.state.hoverRectangle][3]
+      );
+      context.stroke();
+    }
+
+    // if (this.state.selectedRectangle > -1) {
+    //   context.strokeStyle = "red";
+    //   context.beginPath();
+    //   context.rect(
+    //     this.state.rectangles[this.state.selectedRectangle][0],
+    //     this.state.rectangles[this.state.selectedRectangle][1],
+    //     this.state.rectangles[this.state.selectedRectangle][2],
+    //     this.state.rectangles[this.state.selectedRectangle][3]
+    //   );
+    //   context.stroke();
+    // }
+
+    for (let i = 0; i < this.state.moves.length; i++) {
+      const move = this.state.moves[i];
+
+      context.fillText(move.name, move.x, move.y);
+
+      for (let j = 0; j < move.sizes.length; j++) {
+        if (
+          this.state.selectMoveRectangleI === i &&
+          this.state.selectMoveRectangleJ === j
+        ) {
+          context.strokeStyle = "red";
+        } else if (
+          this.state.hoverMoveRectangleI === i &&
+          this.state.hoverMoveRectangleJ === j
+        ) {
+          context.strokeStyle = "blue";
+        } else {
+          context.strokeStyle = "black";
+        }
+
+        context.beginPath();
+        context.rect(move.sizes[j].x, move.sizes[j].y, move.width, move.height);
+        context.stroke();
+
+        if (move.sizes[j].rectangle > -1) {
+          const imageData = this.state.context.getImageData(
+            this.state.rectangles[move.sizes[j].rectangle][0],
+            this.state.rectangles[move.sizes[j].rectangle][1],
+            this.state.rectangles[move.sizes[j].rectangle][2],
+            this.state.rectangles[move.sizes[j].rectangle][3]
+          );
+
+          this.state.context.putImageData(
+            imageData,
+            move.sizes[j].x,
+            move.sizes[j].y
+          );
+        }
+      }
+    }
+  }
+
   distance(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
   }
@@ -240,69 +360,228 @@ export default class App extends Component {
 
   render() {
     return (
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: "flex" }}>
         <canvas
-          ref='canvas'
+          ref="canvas"
           width={500}
           height={window.innerHeight}
           onMouseMove={event => {
-            // console.log(this.state.rectangles, event.clientX, event.clientY);
+            if (!this.rect) {
+              return;
+            }
 
-            const canvas = this.refs.canvas;
-            const context = canvas.getContext('2d');
+            const x = (event.clientX - this.rect.left) * this.scaleX;
+            const y = (event.clientY - this.rect.top) * this.scaleY;
 
-            const x = (event.clientX - this.rect.left) * this.scaleX; // scale mouse coordinates after they have
-            const y = (event.clientY - this.rect.top) * this.scaleY; // been adjusted to be relative to element
+            this.setState({
+              hoverRectangle: -1,
+              hoverMoveRectangleI: -1,
+              hoverMoveRectangleJ: -1
+            });
 
-            // const x = event.clientX;
-            // const y = event.clientY;
-
-            // context.strokeStyle = 'red';
-            // context.beginPath();
-            // context.rect(x, y, 2, 2);
-            // context.stroke();
-
-            for (const rectangle of this.state.rectangles) {
+            for (let i = 0; i < this.state.rectangles.length; i++) {
+              const rectangle = this.state.rectangles[i];
               if (
                 x >= rectangle[0] &&
                 x <= rectangle[0] + rectangle[2] &&
                 y >= rectangle[1] &&
                 y <= rectangle[1] + rectangle[3]
               ) {
-                // console.log(rectangle);
-                context.beginPath();
-                context.rect(rectangle[0], rectangle[1], rectangle[2], rectangle[3]);
-                context.stroke();
+                this.setState({ hoverRectangle: i });
                 break;
               }
             }
-            // const pixelData = this.refs.canvas.getContext('2d').getImageData(event.clientX, event.clientY, 1, 1).data;
-            // console.log(pixelData);
+
+            for (let i = 0; i < this.state.moves.length; i++) {
+              const move = this.state.moves[i];
+              for (let j = 0; j < move.sizes.length; j++) {
+                const size = move.sizes[j];
+
+                if (
+                  x >= size.x &&
+                  x <= size.x + move.width &&
+                  y >= size.y &&
+                  y <= size.y + move.height
+                ) {
+                  this.setState({
+                    hoverMoveRectangleI: i,
+                    hoverMoveRectangleJ: j
+                  });
+                  break;
+                }
+              }
+            }
+          }}
+          onMouseDown={event => {
+            if (!this.rect) {
+              return;
+            }
+
+            const x = (event.clientX - this.rect.left) * this.scaleX;
+            const y = (event.clientY - this.rect.top) * this.scaleY;
+
+            for (let i = 0; i < this.state.rectangles.length; i++) {
+              const rectangle = this.state.rectangles[i];
+              if (
+                x >= rectangle[0] &&
+                x <= rectangle[0] + rectangle[2] &&
+                y >= rectangle[1] &&
+                y <= rectangle[1] + rectangle[3]
+              ) {
+                console.log(
+                  this.state.selectMoveRectangleI,
+                  this.state.selectMoveRectangleJ,
+                  i
+                );
+                // this.setState(prevState => ({
+                //   selectMoveRectangleI: -1,
+                //   selectMoveRectangleJ: -1
+                //   // selectedRectangle: prevState.selectedRectangle === i ? -1 : i
+                // }));
+
+                const imageData = this.state.context.getImageData(
+                  rectangle[0],
+                  rectangle[1],
+                  rectangle[2],
+                  rectangle[3]
+                );
+
+                const moves = Array.from(this.state.moves);
+                if (
+                  this.state.moves[this.state.selectMoveRectangleI].width <
+                  imageData.width
+                ) {
+                  moves[this.state.selectMoveRectangleI].width =
+                    imageData.width;
+                }
+
+                if (
+                  this.state.moves[this.state.selectMoveRectangleI].height <
+                  imageData.height
+                ) {
+                  moves[this.state.selectMoveRectangleI].height =
+                    imageData.height;
+                }
+
+                moves[this.state.selectMoveRectangleI].sizes[
+                  this.state.selectMoveRectangleJ
+                ].rectangle = i;
+
+                this.setState({
+                  moves
+                });
+                break;
+              }
+            }
+
+            for (let i = 0; i < this.state.moves.length; i++) {
+              const move = this.state.moves[i];
+              for (let j = 0; j < move.sizes.length; j++) {
+                const size = move.sizes[j];
+
+                if (
+                  x >= size.x &&
+                  x <= size.x + move.width &&
+                  y >= size.y &&
+                  y <= size.y + move.height
+                ) {
+                  this.setState({
+                    selectMoveRectangleI: i,
+                    selectMoveRectangleJ: j
+                  });
+                  break;
+                }
+              }
+            }
           }}
         />
         <div>
           <div>
             <label>Width: {this.state.width}</label>
             <input
-              type='number'
+              type="number"
               value={this.state.width}
               onChange={event => {
-                this.setState({ width: event.target.value }, this.reload);
+                this.setState(
+                  { width: event.target.value },
+                  this.updateRectangles
+                );
               }}
             />
           </div>
           <div>
             <label>Height: {this.state.height}</label>
             <input
-              type='number'
+              type="number"
               value={this.state.height}
               onChange={event => {
-                this.setState({ height: event.target.value }, this.reload);
+                this.setState(
+                  { height: event.target.value },
+                  this.updateRectangles
+                );
               }}
             />
           </div>
+          <div>
+            <label>Moves: {this.state.moves.length}</label>
+            <input
+              type="number"
+              value={this.state.newMove}
+              onChange={event => {
+                this.setState({ newMove: event.target.value });
+              }}
+            />
+            <button
+              onClick={() => {
+                const sizes = [];
+                const i = this.state.moves.length;
+
+                for (let j = 0; j < this.state.newMove; j++) {
+                  sizes.push({
+                    x: 250 + 40 * j,
+                    y: 30 + 60 * i
+                  });
+                }
+
+                this.setState(
+                  prevState => ({
+                    moves: [
+                      ...prevState.moves,
+                      {
+                        name: prevState.moves.length + 1,
+                        x: 250,
+                        y: 20 + 60 * prevState.moves.length,
+                        size: this.state.newMove,
+                        width: 30,
+                        height: 30,
+                        sizes
+                      }
+                    ]
+                    // newMove: 1
+                  }),
+                  this.reload
+                );
+              }}
+            >
+              Add new move
+            </button>
+          </div>
+          {this.state.moves.map((m, i) => (
+            <div key={i}>
+              <label>
+                Size {i + 1}: {m.size}
+              </label>
+
+              {/* <button>Select sprite</button> */}
+            </div>
+          ))}
         </div>
-        <img ref='image' src={sprite} alt='sprite' style={{ visibility: 'hidden' }} />
+        <img
+          ref="image"
+          src={sprite}
+          alt="sprite"
+          style={{ visibility: "hidden" }}
+        />
       </div>
     );
   }
